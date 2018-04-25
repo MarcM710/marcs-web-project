cb667ea2118c49c69ce0c747aae9092b8291e3f5
MarcM710 <38624796+MarcM710@users.noreply.github.com>
2018-04-25 15:03:55 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 //    This link is the documentation for ExpressJS, but it might be a little
 //	  complicated to read for you right now.
 const express = require('express');  
+const PORT = process.env.PORT || 5000
 
 // This creates our web app.
 const app = express();
@@ -52,7 +53,7 @@ app.get('/route', function(requestObject, responseObject) {
 // Feel free to change it to another number, but doesn't change
 // anything. Just pick anything higher than 3000. (Other numbers
 // might be reserved for the operating system.
-app.listen(8080,function() {
-	console.log('Server started running on port 8080!');
+app.listen(PORT,function() {
+	console.log('Server started running on port PORT!');
 });
 
